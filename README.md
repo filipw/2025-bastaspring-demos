6f1e16257150f246c9856a30d5581aa57e2fd560
filipw <filip@strathweb.com>
2025-09-02 20:46:07 +0200

    first set of demos

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2025 Basta Spring Demos
+
+Demos for my session "Post-Quantum Cryptography for .NET Developers" at Basta Spring 2025 in Mainz. 
---
+++ src/Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using Spectre.Console;
+
+var demo = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Choose the [green]demo[/] to run?")
+        .AddChoices(
+        [
+            "ML-KEM (BouncyCastle)", "ML-DSA (BouncyCastle)", "ML-KEM (Windows API)", "ML-DSA (Windows API)"
+        ]));
+
+switch (demo)
+{
+    case "ML-KEM (BouncyCastle)":
+        BouncyCastleDemo.RunMlKem();
+        break;
+    case "ML-DSA (BouncyCastle)":
+        BouncyCastleDemo.RunMldsa();
+        break;
+    case "ML-DSA (Windows API)":
+        WindowsDemo.RunMlDsa();
+        break;
+    case "ML-KEM (Windows API)":
+        WindowsDemo.RunMlKem();
+        break;
+    default:
+        Console.WriteLine("Nothing selected!");
+        break;
+}
+
+public static class FormatExtensions
+{
+    public static string PrettyPrint(this byte[] bytes)
+    {
+        var base64 = Convert.ToBase64String(bytes);
+        return base64.Length > 50 ? $"{base64[..25]}...{base64[^25..]}" : base64;
+    }
+}
